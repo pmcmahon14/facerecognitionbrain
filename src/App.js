98401311275d2b6cbc5d8641657be13df119f653
Patrick McMahon <pmcmahon14@gmail.com>
2018-12-28 14:21:44 -0800

    added signin folder and file, modified to switch between sign in and sign out, styled sign in box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -31,6 +32,7 @@ class App extends Component {
             input: '',
             imageUrl: '',
             box: {},
+            route: 'signin'
         }
     }
 
@@ -66,17 +68,29 @@ class App extends Component {
                 .catch(err => console.log(err));
     };
 
+    onRouteChange = (route) => {
+        this.setState({route: route});
+    };
+
   render() {
     return (
       <div className="App">
           <Particles className='particles'
               params={particlesOptions}
           />
-          <Navigation/>
-          <Logo/>
-          <Rank/>
-          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+          <Navigation onRouteChange={this.onRouteChange}/>
+          {this.state.route === 'signin'
+              ? <Signin onRouteChange={this.onRouteChange}/>
+              : <div>
+                  <Logo/>
+                  <Rank/>
+                  <ImageLinkForm
+                      onInputChange={this.onInputChange}
+                      onButtonSubmit={this.onButtonSubmit}
+                  />
+                  <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+              </div>
+          }
       </div>
     );
   }
